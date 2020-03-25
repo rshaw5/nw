017daef31c06d32b0b9a3a8504f16d899ea87767
Ubuntu Burner <VicisVitium@gmail.com>
2020-03-25 10:03:48 -0700

    Add chrome/firefox capabilities with headless options

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -1,20 +1,66 @@
 module.exports = {
     src_folders : ["tests"],
 
-    webdriver : {
-        start_process: true,
-        server_path: require('chromedriver').path,
-        port: 9515
-    },
-
     test_settings : {
-        default : {
+        // default : {
+        //     desiredCapabilities: {
+        //         browserName: 'chrome',
+        //         chromeOptions: {
+        //             args: [ '--headless' ]
+        //         }
+        //     }
+        // },
+
+        chrome : {
+            desiredCapabilities: {
+                browserName: 'chrome'
+            },
+            webdriver : {
+                start_process: true,
+                server_path: require('chromedriver').path,
+                port: 9515
+            }
+        },
+
+        chromeHeadless : {
             desiredCapabilities: {
                 browserName: 'chrome',
                 chromeOptions: {
                     args: [ '--headless' ]
                 }
+            },
+            webdriver : {
+                start_process: true,
+                server_path: require('chromedriver').path,
+                port: 9515
             }
-        }
+        },
+
+        firefox : {
+            desiredCapabilities: {
+                browserName: 'firefox'
+            },
+            webdriver : {
+                start_process: true,
+                server_path: require('geckodriver').path,
+                port: 4444
+            }
+        },
+
+        firefoxHeadless : {
+            desiredCapabilities: {
+                browserName: 'firefox',
+                alwaysMatch: {
+                    'moz:firefoxOptions': {
+                        args: [ '-headless' ]
+                    }
+                }
+            },
+            webdriver : {
+                start_process: true,
+                server_path: require('geckodriver').path,
+                port: 4444
+            }
+        },
     }
 }
